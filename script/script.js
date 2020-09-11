031750cb9aedb75aeb447206f3254a0d2c8751f0
CHANDAN RAJ <69450631+chandan95raj@users.noreply.github.com>
2020-09-11 21:51:35 +0530

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,27 @@
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+  document.getElementById("cntb").style.backgroundColor = "white";
+
+  
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+  document.getElementById("cntb").style.backgroundColor = "";
+}
+
+
+function t(){
+
+    var alldate = new Date();
+    var tim = alldate.toLocaleTimeString();
+    var day = alldate.getDate();
+    var month = alldate.getMonth()+1;
+    var year = alldate.getFullYear();
+    var ti = document.getElementById("time");
+    ti.innerHTML = day+ "/" + month + "/" + year + " . " +  tim + " " ;
+}
+
+
+setInterval(t,100);
+
